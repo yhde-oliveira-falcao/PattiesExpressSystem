a74ff74181fbf7ab492bb03284a737e6459cd039
Yuri Falcao <yhofalcao@gmail.com>
2020-12-27 17:30:29 -0500

    some initial changes

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-
-
+var mongoose = require("mongoose");
+const ehbs = require('express-handlebars');
 
 
 // setup a 'route' to listen on the default url path
