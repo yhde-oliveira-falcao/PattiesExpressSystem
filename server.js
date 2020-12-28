b211b892e2be1e40bb94864e90f9669aacb2e845
Yuri Falcao <yhofalcao@gmail.com>
2020-12-28 00:32:34 -0500

    more changes

--- server.js
+++ server.js
@@ -23,6 +23,36 @@ mongoose.connect(config.dbconn, {useNewUrlParser: true, useUnifiedTopology: true
 app.use(express.static("views"));
 app.use(express.static("public"));
 
+app.use(clientSessions({
+    cookieName: "Log Session",
+    secret: "Data Storage Website",
+    duration: 40*60*1000,
+    activeDuration: 1000*60*40
+}));
+//Keep logged in!!!??? YES! With google login
+
+app.use(bodyParser.urlencoded({extended: false }));
+
+
+//SECURITY===============
+function ensureLogin(req, res, next) {
+    if (!req.session.user) {
+      res.redirect("/login");
+    } else {
+      next();
+    }
+};
+function ensureAdmin(req, res, next) {
+    if (!user.isAdmin) {
+      res.redirect("/login");
+    } else {
+      next();
+    }
+};//?????
+//INCLUDE A FUNCTION TO SECURE THE DATA INPUT.
+
+
+
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
     res.send("<h1> Hello World! </h1>");
